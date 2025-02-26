478feccb774e146d958f317a7ba70132fa72eabe
caoliangchen <caoliangchen@didachuxing.com>
2025-02-26 11:50:07 +0800

    add:处理编译问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
@@ -8,3 +7,10 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
 }
+
+//buildscript {
+//    dependencies {
+//        // 降级 AGP 版本到 8.6.0
+//        classpath("com.android.tools.build:gradle:8.6.0")
+//    }
+//}
